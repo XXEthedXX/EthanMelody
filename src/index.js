1d8db822277f80fdb4c31ecb12ebdbb2fb829937
Access Point <ebalikalaba@my.stlcc.edu>
2023-08-21 15:55:52 -0500

    Locally working with default commands!

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
-console.log('line 7: ');
 
 // Create a new client instance / initialize commands collection
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-// changed absolute path to relative path
-console.log('line 14: ');
 const foldersPath = path.join(__dirname, '..', 'commands');
 console.log('foldersPath: ', foldersPath);
 const commandFolders = fs.readdirSync(foldersPath);
 
-// dynamically retrtieve your command files
+// dynamically retrieve your command files
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -34,42 +31,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-// create a listener for client#event:interactionCreate event that runs when code gets and interaction
-client.on(Events.InteractionCreate, interaction => {
-	console.log(interaction);
-});
+const eventsPath = path.join(__dirname, '..', 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// not every interaction is a slash command, handle MessageComponent interactions
-// find matching commands from client.commands
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commmands.get(interaction.commandName);
-
-	if (!command) {
-		console.error('No command matching ${interaction.commandName} was found.');
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
 	}
-
-	try {
-		await command.execute(interaction);
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-		else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-// When client is ready, run once
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+}
 
 // Log in to Discord
 client.login(token);
